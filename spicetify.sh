fb8ac2ba3467a4b740d56d88eee2140d9d12913c
Dipan <43346471+themagicalmammal@users.noreply.github.com>
2020-11-13 01:21:41 +0530

    Replace username

--- spicetify.sh
+++ spicetify.sh
@@ -2,9 +2,9 @@ echo "this work for spotify installed using apt or snap not for flatpak"
 curl -fsSL https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.sh | sh
 sudo chmod a+wr /usr/share/spotify
 sudo chmod a+wr /usr/share/spotify/Apps -R
-export SPICETIFY_INSTALL="/home/themagicallaptop/spicetify-cli"
+export SPICETIFY_INSTALL="$(xdg-user-dir HOME)/spicetify-cli"
 export PATH="$SPICETIFY_INSTALL:$PATH"
-echo 'export SPICETIFY_INSTALL="/home/themagicallaptop/spicetify-cli' >>~/.profile
+echo 'export SPICETIFY_INSTALL="$(xdg-user-dir HOME)/spicetify-cli' >>~/.profile
 echo 'export PATH="$SPICETIFY_INSTALL:$PATH' >>~/.profile
 spicetify
 spicetify backup apply enable-devtool

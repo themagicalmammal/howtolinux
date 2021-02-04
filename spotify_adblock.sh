fdc77833f612f73a12b5e6669222848dd738807b
Dipan <43346471+themagicalmammal@users.noreply.github.com>
2021-02-04 14:16:47 +0530

    Script: Simplified

--- spotify_adblock.sh
+++ spotify_adblock.sh
@@ -1,13 +1,6 @@
-echo "Script has started! Please wait a bit..."
-sudo apt install git wget tar make gcc libc-dev
-git clone https://github.com/abba23/spotify-adblock-linux.git
-cd spotify-adblock-linux
-wget -O cef.tar.bz2 https://cef-builds.spotifycdn.com/cef_binary_88.1.6%2Bg4fe33a1%2Bchromium-88.0.4324.96_linux64_minimal.tar.bz2
-tar -xf cef.tar.bz2 --wildcards '*/include' --strip-components=1
-make
-sudo make install
-cd ..
-rm -rf spotify-adblock-linux
+echo "Script has started!"
+wget https://github.com/abba23/spotify-adblock-linux/releases/download/v1.2/spotify-adblock.so
+sudo mv spotify-adblock.so /usr/local/lib/spotify-adblock.so
 wget https://raw.githubusercontent.com/themagicalmammal/howtopopbuntu/master/icons/spotify.desktop
 sudo mv spotify.desktop /usr/share/applications/spotify.desktop
 echo "Install is successful, please submit an issue here: https://github.com/themagicalmammal/howtopopbuntu/issues"
